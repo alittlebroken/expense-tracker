c61080cff70f96c9b629a8bc9fcdca371cbe1764
Paul Lockyer <plockyer@googlemail.com>
2024-04-02 22:22:50 +0100

    added methd for showing all transactions

--- src/Account.py
+++ src/Account.py
@@ -12,3 +12,26 @@ class Account:
         self.name = name
         self.balance = balance
         self.transactions = []
+
+    """show_all_transactions
+
+    Displays the list of transactions this account has
+    
+    Keyword arguments: None
+    Return: Nothing
+    """
+    def show_all_transactions(self):
+
+        # Counter for ID of transaction
+        counter = 0
+        print("All transactions for account: {}".format(self.name))
+        for transaction in self.transactions:
+            print("{}. {} - £{} - {}".format(
+                counter,
+                transaction.name,
+                transaction.amount,
+                transaction.category
+            ))
+
+    
+    
