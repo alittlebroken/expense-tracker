7fbccb7f767299c98220c8f4ed415cc97fac3040
Paul Lockyer <plockyer@googlemail.com>
2024-04-03 15:27:48 +0100

    updated show_menu method for more options

--- expenses.py
+++ expenses.py
@@ -12,7 +12,7 @@ Displays the applications main menu
 Keyword arguments: None
 Return: None
 """
-def show_menu():
+def show_menu(app_title):
 
     # Controls the while loop so we can end the program
     # if need be
@@ -21,30 +21,50 @@ def show_menu():
     # Run the program until the user decides to quit
     while running:
 
-        print("\n-------------------")
-        print("Python Expenses App")
-        print("-------------------\n")
-        print("You can perform the following:\n")
-        print("1. Add expense")
-        print("2. Remove expense")
-        print("3. List expenses")
-        print("4. Quit application")
+        print("\n")
+        print("-" * len(app_title))
+        print(app_title)
+        print("-" * len(app_title))
+
+        print("Choose an option from below:\n")
+        
+        print("Accounts:")
+        print("1. List")
+        print("2. Select")
+        print("3. New")
+        print("4. Remove")
         print("\n")
 
-        option = input("> ")
+        print("Transactions:")
+        print("5. List")
+        print("6. Add")
+        print("7. Remove")
+        print("\n")
+        
+        print("8. Quit application")
+        print("\n")
 
-        if option == "1":
-            add_expense()
-        elif option == "2":
-            remove_expense()
-        elif option == "3":
-            list_expenses()
-        elif option == "4":
-            running = False
-        else:
-            print("Please try again that option is not recognised")
-    
-    print("\nThank you for using the Python expenses app")
+        option = int(input("> "))
+
+        match option:
+            case 1:
+                pass
+            case 2:
+                pass
+            case 3:
+                pass
+            case 4:
+                pass
+            case 5:
+                pass
+            case 6:
+                pass
+            case 7:
+                pass
+            case 8:
+                running = False
+            case _:
+                print("Sorry. That option is not recognised. Please try again.")
         
 
 """list_expenses
@@ -133,4 +153,4 @@ run the application
 """
 if __name__ == "__main__":
 
-    show_menu()
+    show_menu("Python Budgeter")
